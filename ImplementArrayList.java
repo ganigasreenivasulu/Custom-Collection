6e8b85ee81bb60d22b8c40ba5d77ab34209df9e9
ganigasreenivasulu <gsreenu9652@gmail.com>
2020-07-25 21:48:00 +0530

    Commit

---
+++ ImplementArrayList.java
@@ -0,0 +1,23 @@
+import ArrayList.*;
+ 
+public class ImplementArrayList
+{
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+ 
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+         
+        list.remove(2);
+        System.out.println(list);
+         
+        
+        System.out.println( list.get(0) );
+        System.out.println( list.get(1) );
+
+        System.out.println(list.size());
+    }
+}
